71d559d299f7b8ab40e8bdc26678645a4a4c5eea
Naoaki Obiki <obiki.wk@gmail.com>
2016-12-16 20:55:35 +0900

    supervisor

--- settings/uwsgi/bootstrap_uwsgi.sh
+++ settings/uwsgi/bootstrap_uwsgi.sh
@@ -7,3 +7,4 @@ sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pyenv virtualenv 3.5.0 uwsgi"
 sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pyenv local uwsgi"
 sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pip install -r packages.txt"
 
+/usr/bin/supervisord -n
