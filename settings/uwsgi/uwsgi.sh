faab5a178dc04722c2e9e94b1c9019372e83bd75
Naoaki Obiki <obiki.wk@gmail.com>
2016-12-17 01:47:49 +0900

    modified:   settings/uwsgi/uwsgi.sh

--- settings/uwsgi/uwsgi.sh
+++ settings/uwsgi/uwsgi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo -i -u 9zilla bash -c "anyenv install pyenv"
-sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pyenv install 3.5.0"
-sudo -i -u 9zilla bash -c "git clone https://github.com/yyuu/pyenv-virtualenv ~/.anyenv/envs/pyenv/plugins/pyenv-virtualenv"
-sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pyenv virtualenv 3.5.0 uwsgi"
-sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pyenv local uwsgi"
-sudo -i -u 9zilla bash -c "cd /etc/uwsgi/ && pip install -r packages.txt"
+sudo chown 1000:1000 ~/.anyenv/envs/
+
+sudo -i -u "#1000" bash -c "anyenv install pyenv"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv install 3.5.0"
+sudo -i -u "#1000" bash -c "git clone https://github.com/yyuu/pyenv-virtualenv ~/.anyenv/envs/pyenv/plugins/pyenv-virtualenv"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv virtualenv 3.5.0 uwsgi"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv local uwsgi"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pip install -r packages.txt"
