ec4005e2da1515a793179e77a9f60bd02e57e917
Naoaki Obiki <obiki.wk@gmail.com>
2016-12-31 17:02:58 +0900

    php

--- settings/php-fpm/php-fpm.sh
+++ settings/php-fpm/php-fpm.sh
@@ -6,4 +6,5 @@ sudo -i -u "#1000" bash -c "anyenv install phpenv"
 sudo -i -u "#1000" bash -c "phpenv install 7.0.14"
 
 sudo -i -u "#1000" bash -c "mv /home/9zilla/.anyenv/envs/phpenv/versions/7.0.14/etc/php-fpm.conf.default /home/9zilla/.anyenv/envs/phpenv/versions/7.0.14/etc/php-fpm.conf"
+sudo -i -u "#1000" bash -c "ln -s /etc/php-fpm/php-fpm.d/virtualdomains.conf /home/9zilla/.anyenv/envs/phpenv/versions/7.0.14/etc/php-fpm.d/virtualdomains.conf"
 
