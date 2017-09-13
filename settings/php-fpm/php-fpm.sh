ce9af48e7eeab0f9f3999d518ed59ed6f43e37ce
Naoaki Obiki <obiki.wk@gmail.com>
2017-09-13 21:15:35 +0900

    php 7.2.0RC1

--- settings/php-fpm/php-fpm.sh
+++ settings/php-fpm/php-fpm.sh
@@ -5,7 +5,7 @@ chown 1000:1000 /home/9zilla/.anyenv/envs/
 sudo -i -u "#1000" bash -c "anyenv install phpenv"
 sudo -i -u "#1000" bash -c "cp /default_configure_options /home/9zilla/.anyenv/envs/phpenv/plugins/php-build/share/php-build/"
 
-sudo -i -u "#1000" bash -c "phpenv install 7.1.9"
+sudo -i -u "#1000" bash -c "phpenv install 7.2.0RC1"
 
-sudo -i -u "#1000" bash -c "mv /home/9zilla/.anyenv/envs/phpenv/versions/7.1.9/etc/php-fpm.conf.default /home/9zilla/.anyenv/envs/phpenv/versions/7.1.9/etc/php-fpm.conf"
-sudo -i -u "#1000" bash -c "ln -s /etc/php-fpm/php-fpm.d/virtualdomains.conf /home/9zilla/.anyenv/envs/phpenv/versions/7.1.9/etc/php-fpm.d/virtualdomains.conf"
+sudo -i -u "#1000" bash -c "mv /home/9zilla/.anyenv/envs/phpenv/versions/7.2.0RC1/etc/php-fpm.conf.default /home/9zilla/.anyenv/envs/phpenv/versions/7.2.0RC1/etc/php-fpm.conf"
+sudo -i -u "#1000" bash -c "ln -s /etc/php-fpm/php-fpm.d/virtualdomains.conf /home/9zilla/.anyenv/envs/phpenv/versions/7.2.0RC1/etc/php-fpm.d/virtualdomains.conf"
