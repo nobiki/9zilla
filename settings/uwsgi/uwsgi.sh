de386014e591fd4b27afb4cee138d335e710257c
Naoaki Obiki <obiki.wk@gmail.com>
2017-09-14 00:34:34 +0900

    python 3.5.0 -> 3.6.2

--- settings/uwsgi/uwsgi.sh
+++ settings/uwsgi/uwsgi.sh
@@ -3,8 +3,8 @@
 chown 1000:1000 /home/9zilla/.anyenv/envs/
 
 sudo -i -u "#1000" bash -c "anyenv install pyenv"
-sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv install 3.5.0"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv install 3.6.2"
 sudo -i -u "#1000" bash -c "git clone https://github.com/yyuu/pyenv-virtualenv /home/9zilla/.anyenv/envs/pyenv/plugins/pyenv-virtualenv"
-sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv virtualenv 3.5.0 uwsgi"
+sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv virtualenv 3.6.2 uwsgi"
 sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pyenv local uwsgi"
 sudo -i -u "#1000" bash -c "cd /etc/uwsgi/ && pip install -r packages.txt"
